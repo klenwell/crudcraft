264b206e9d4a30125fc0f6c769acdf9a224c7a76
Tom Atwell <klenwell@gmail.com>
2020-03-29 19:52:33 -0700

    Adds crud model tests for attribute requirements.

--- app-engine/tests/models/test_crud.py
+++ app-engine/tests/models/test_crud.py
@@ -5,6 +5,7 @@ To run individually:
 
     nosetests -c nose.cfg tests/models/test_crud.py
 """
+from google.appengine.ext import db
 from google.appengine.api import users
 
 from models.guest import Guest
@@ -20,13 +21,40 @@ class GuestModelTest(AppEngineModelTest):
     #
     # Class Tests
     #
+    def test_expects_new_crud_to_require_message(self):
+        # Arrange
+        email = 'user@gmail.com'
+        MockIdentityService.stub_app_engine_user(self, email=email)
+        app_engine_user = users.get_current_user()
+        creator = Guest.app_engine_user(app_engine_user)
+        message = None
+
+        # Act
+        with self.assertRaises(db.BadValueError) as c:
+            crud = Crud.create(creator, message)
+
+        # Assert
+        self.assertEqual(str(c.exception), "Entity has uninitialized properties: message")
+
+    def test_expects_new_crud_to_require_creator(self):
+        # Arrange
+        creator = None
+        message = 'This is a crud test.'
+
+        # Act
+        with self.assertRaises(AttributeError) as c:
+            crud = Crud.create(creator, message)
+
+        # Assert
+        self.assertEqual(str(c.exception), "'NoneType' object has no attribute 'key'")
+
     def test_expects_to_create_new_crud(self):
         # Arrange
         email = 'user@gmail.com'
         MockIdentityService.stub_app_engine_user(self, email=email)
         app_engine_user = users.get_current_user()
         creator = Guest.app_engine_user(app_engine_user)
-        message = 'This is a crud test'
+        message = 'This is a crud test.'
 
         # Assume
         self.assertEqual(creator.email, email)
