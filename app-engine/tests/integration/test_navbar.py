82df95fbd21583be3effd013ed3baeebcc49664c
Tom Atwell <klenwell@gmail.com>
2020-05-09 12:25:39 -0700

    Adds test from cruds tab in navbar.

--- app-engine/tests/integration/test_navbar.py
+++ app-engine/tests/integration/test_navbar.py
@@ -11,10 +11,6 @@ from tests.helper import (AppEngineControllerTest, MockIdentityService, parse_ht
 
 
 class NavbarIntegrationTest(AppEngineControllerTest):
-    #
-    # Tests
-    #
-    ## Permissions
     def test_should_display_navbar_to_any_guest(self):
         # Arrange
         client = pages_controller.test_client()
@@ -31,3 +27,20 @@ class NavbarIntegrationTest(AppEngineControllerTest):
         # Assert
         self.assertEqual(response.status_code, 200, response.data)
         self.assertIsNotNone(navbar, html)
+
+    def test_should_display_cruds_tab_in_navbar_to_any_guest(self):
+        # Arrange
+        client = pages_controller.test_client()
+        unauthenticated_guest = MockIdentityService.unauthenticated_guest(self)
+
+        # Assume
+        self.assertFalse(unauthenticated_guest.is_authenticated())
+
+        # Act
+        response = client.get('/')
+        html = parse_html(response.data)
+        cruds_tab = html.select_one('nav.navbar li.cruds')
+
+        # Assert
+        self.assertEqual(response.status_code, 200, response.data)
+        self.assertIsNotNone(cruds_tab, html)
